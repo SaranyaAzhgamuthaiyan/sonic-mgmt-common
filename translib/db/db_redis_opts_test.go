1acd4eb3af592ec588ae2c749f3b63524a4b63e2
Saranya Azhagamuthaiyan <sa@infinera.com>
2024-05-13 13:23:54 +0530

    Adding missed file

--- translib/db/db_redis_opts_test.go
+++ translib/db/db_redis_opts_test.go
@@ -20,6 +20,7 @@
 package db
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -45,6 +46,73 @@ func TestReadFromDBRedisOpts(t *testing.T) {
 
 }
 
+func TestAdjustRedisOpts(t *testing.T) {
+	for _, dbName := range GetMultiDbNames() {
+		adjustValue := adjustRedisOpts(&Options{
+			DBNo:               ConfigDB,
+			InitIndicator:      "",
+			TableNameSeparator: "|",
+			KeySeparator:       "|",
+			DisableCVLCheck:    true,
+		}, dbName)
+		/*
+		   adjustDbValues for host = &{unix /var/run/redis/redis.sock <nil> <nil>  4 0 0s 0
+		   s 0s 10s 11s 1 0 0s 0s 0s 0s false <nil>}
+		*/
+		t.Run("AdjustRedisOpts of "+dbName, func(t *testing.T) {
+			switch dbName {
+			case "host":
+				t.Run("HostConnection", func(t *testing.T) {
+					//Redis</var/run/redis0/redis.sock db:4>
+					if adjustValue.Addr != "/var/run/redis/redis.sock" {
+						t.Error("GetDbSock fails in host connection")
+					}
+				})
+			case "asic0":
+				t.Run("Asic0Connection", func(t *testing.T) {
+					if adjustValue.Addr != "/var/run/redis0/redis.sock" {
+						t.Error("GetDbSock fails in asic0 connection")
+					}
+				})
+			case "asic1":
+				t.Run("Asic1Connection", func(t *testing.T) {
+					if adjustValue.Addr != "/var/run/redis1/redis.sock" {
+						t.Error("GetDbSock fails in asic1 connection")
+					}
+				})
+			case "asic2":
+				t.Run("Asic2Connection", func(t *testing.T) {
+					if adjustValue.Addr != "/var/run/redis2/redis.sock" {
+						t.Error("GetDbSock fails in asic2 connection")
+					}
+				})
+			case "asic3":
+				t.Run("Asic3Connection", func(t *testing.T) {
+					if adjustValue.Addr != "/var/run/redis3/redis.sock" {
+						t.Error("GetDbSock fails in asic3 connection")
+					}
+				})
+			default:
+				t.Errorf("Unexpected database name: %s", dbName)
+			}
+
+		})
+		fmt.Printf("adjustDbValues for %s = %v adjustVale = %v\n", dbName, adjustValue, adjustValue.Addr)
+	}
+}
+
+func TestReadRedis(t *testing.T) {
+	//FEATURE|gnmi
+	for _, dbName := range GetMultiDbNames() {
+		readData, _ := readRedis("FEATURE|gnmi", dbName)
+		if _, ok := readData["auto_restart"]; ok {
+			fmt.Printf("Data is present in %s\n", dbName)
+		} else {
+			fmt.Printf("Data is not present in %s\n", dbName)
+		}
+		//fmt.Printf("readredis Values for %s = %v and err = %v\n", dbName, readData, err)
+	}
+}
 func compareRedisOptsString2Struct(t *testing.T, optsS string, opts *redis.Options) {
 	setGoRedisOpts(optsS)
 	if !reflect.DeepEqual(dbRedisOptsConfig.opts, *opts) {
